3aa985514360a655f5669b0fc110c11781963280
Tincho <tinchoz8426@gmail.com>
2023-07-06 10:25:32 -0300

    feat: navbar linked and button to top

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let buttonToTop = document.getElementById("buttonToTop")
+
+// Se puede hacer de esta forma
+window.addEventListener("scroll", scrollFunction)
+// O de esta otra forma
+/* window.onscroll = function () {
+    scrollFunction()
+} */
+
+/**
+ * cambia el display del boton de none a block cuandola barra de desplazamiento pasa los 20 px
+ * @author "Juani, Jaime, Tincho"
+ * @param Nothing
+ * @return {void} Nothing
+ */
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        buttonToTop.style.display = "block";
+    } else {
+        buttonToTop.style.display = "none";
+    }
+}
+
+/**
+ * // cuando se presiona el boton la barra de desplazamiento vuelve a arriba.
+ * @author "Juani, Jaime, Tincho"
+ * @param Nothing
+ * @return {void} Nothing
+ */ 
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
